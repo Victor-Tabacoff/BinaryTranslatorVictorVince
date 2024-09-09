e0226c63e77b6bf1d5c0f439797c05b77efa2a1e
Victor Tabacoff <victortabacoff2007@gmail.com>
2024-09-09 09:41:42 -0500

    Initial Commit

---
+++ BinaryTranslatorVictorVince/BinaryTranslatorVictorVinceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryTranslatorVictorVinceApp.swift
+//  BinaryTranslatorVictorVince
+//
+//  Created by Victor B. Tabacoff on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BinaryTranslatorVictorVinceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BinaryTranslatorVictorVince/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BinaryTranslatorVictorVince
+//
+//  Created by Victor B. Tabacoff on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
